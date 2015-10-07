ec25b0fef5999a29d963ea198d913d980f92dd38
ganbaru4 <shinyaminakuchi@mizuguchishinya-no-MacBook-Pro.local>
2015-10-07 21:28:25 +0900

    test

---
+++ Assets/chase.cs
@@ -0,0 +1,22 @@
+﻿  using UnityEngine;
+using System.Collections;
+
+public class chase : MonoBehaviour {
+
+	public Transform target;//oikakeru taishou - inspector kara touroku
+	public float speed = 0.1f;//idou speed
+	private Vector3 vec;
+
+
+	// Use this for initialization
+	void Start () {
+//		target = GameObject.Find("escapeB").transform; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		transform.rotation = Quaternion.LookRotation (target.position - transform.position);
+		transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (target.position - transform.position), 0.3f);
+		transform.position += transform.forward * speed;
+	}
+}
---
+++ Assets/createBall.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class createBall : MonoBehaviour {
+
+	public GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+		GameObject instance = (GameObject)Instantiate(obj);
+		instance.transform.position = gameObject.transform.position;
+		instance.transform.rotation = gameObject.transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if(Input.GetButton("Jump")){//Input.GetMouseButtonDown(0)
+//			GameObject instance = (GameObject)Instantiate(obj);
+//			instance.transform.position = gameObject.transform.position;
+//			instance.transform.rotation = gameObject.transform.rotation;
+//
+//		}
+	}
+}
